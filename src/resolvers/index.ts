d341388c8f8bde7faf63e119a6c5c672a04fbe5e
Roman <rcheskidov@atlassian.com>
2021-05-17 21:22:09 -0700

    perf(devops): develop a new logic for getAnalyticsService, I

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,11 +1,7 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient } = require('mongodb')
 
-import { getAccessData } from "../shared/getAccessData";
-import { getAnalyticsService } from "../services/getAnalyticsService";
-import { saveAnalyticsService } from "../services/saveAnalyticsService";
-
-let dbAccessData = getAccessData();
-dbAccessData = { ...dbAccessData, MongoClient };
+import { getAnalyticsService } from '../services/getAnalyticsService'
+import { saveAnalyticsService } from '../services/saveAnalyticsService'
 
 // Resolvers define the technique for fetching the types in the
 // schema.  We'll retrieve books from the "books" array above.
@@ -15,9 +11,9 @@ const resolvers = {
      * @example "query": "mutation SaveUserAnalytics($analyticsInput: AnalyticsInput!){saveUserAnalytics(analyticsInput: $analyticsInput){ n }}"
      */
     saveAnalytics: (parent, args, context, info) => {
-      const { ip } = context;
-      const dataInput = { ...args, ip };
-      return saveAnalyticsService(dataInput);
+      const { ip } = context
+      const dataInput = { ...args, ip }
+      return saveAnalyticsService(dataInput)
     },
   },
   Query: {
@@ -25,10 +21,10 @@ const resolvers = {
      * @example {"operationName":false,"variables":{},"query":"{getAnalytics(dateFrom:\"2019\/05\/20\",dateTo:\"2019\/05\/30\"){finish,start,initData{width,height,search,pathname,hostname,href,referrer,ip}target{level,name},topics}}"}
      */
     getAnalytics: (parent, args, context, info) => {
-      const dataInput = { ...args };
-      return getAnalyticsService(dbAccessData, dataInput);
+      const dataInput = { ...args }
+      return getAnalyticsService(dataInput)
     },
   },
-};
+}
 
-module.exports = resolvers;
+module.exports = resolvers
---
+++ src/services/mockRepsonse.ts
@@ -0,0 +1,110 @@
+export const mockResponse: any[] = [
+  {
+    analyticsID: '1_TCyFYolEryXOnKqqTAbTw',
+    dateCreate: 1621309388288,
+    dateUpdate: 1621309432873,
+    initData: {
+      width: 1000,
+      height: 500,
+      search: 'String',
+      pathname: 'String',
+      hostname: 'String',
+      href: 'String',
+      referrer: 'String',
+    },
+    topics: [
+      {
+        name: 'String',
+        pathname: 'String',
+      },
+      {
+        name: 'String',
+        pathname: 'String',
+      },
+      {
+        name: 'String',
+        pathname: 'String',
+      },
+    ],
+    events: [
+      {
+        type: 'String',
+        name: 'String',
+        value: 'String',
+        pathname: 'String',
+      },
+      {
+        type: 'String',
+        name: 'String',
+        value: 'String',
+        pathname: 'String',
+      },
+    ],
+    targets: [
+      {
+        level: '1',
+        name: 'String',
+        pathname: 'String',
+      },
+      {
+        level: '1',
+        name: 'String',
+        pathname: 'String',
+      },
+    ],
+  },
+  {
+    analyticsID: '2_TCyFYolEryXOnKqqTAbTw',
+    dateCreate: 1621309388288,
+    dateUpdate: 1621309432873,
+    initData: {
+      width: 1000,
+      height: 500,
+      search: 'String',
+      pathname: 'String',
+      hostname: 'String',
+      href: 'String',
+      referrer: 'String',
+    },
+    topics: [
+      {
+        name: 'String',
+        pathname: 'String',
+      },
+      {
+        name: 'String',
+        pathname: 'String',
+      },
+      {
+        name: 'String',
+        pathname: 'String',
+      },
+    ],
+    events: [
+      {
+        type: 'String',
+        name: 'String',
+        value: 'String',
+        pathname: 'String',
+      },
+      {
+        type: 'String',
+        name: 'String',
+        value: 'String',
+        pathname: 'String',
+      },
+    ],
+    targets: [
+      {
+        level: '1',
+        name: 'String',
+        pathname: 'String',
+      },
+      {
+        level: '1',
+        name: 'String',
+        pathname: 'String',
+      },
+    ],
+  },
+]
